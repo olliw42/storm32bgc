db6b3abfac4c1f2e3c5ee90e4b5e44d8aa7900d0
olliw42 <waldmanns@gmx.de>
2014-03-20 19:16:18 +0100

    o323bgc v0.25 released

---
+++ firmware binaries & gui/o323bgc-release-v025-v20140320/o323BGCTool_Run.bat
@@ -0,0 +1,4 @@
+"ST\STMFlashLoader\STMFlashLoaderOlliW.exe" -c --pn 3  --br 115200 -ow -e --all -d --fn "o323BgcFirmwareFiles\o323bgc_v025e_storm32bgc_v110_f103rc.hex" --v -r --a 8000000
+
+@echo.
+@pause
