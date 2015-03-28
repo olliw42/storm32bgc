3c13854c580bfc499367e378dc02ce8f72f4822d
olliw42 <waldmanns@gmx.de>
2015-03-28 16:03:47 +0100

    release firmware v0.68

---
+++ firmware binaries & gui/o323bgc-release-v068-v20150328/o323BGCTool_Run.bat
@@ -0,0 +1,4 @@
+"bin\ST\STLink\st-link_cli.exe" -ME
+"bin\ST\STLink\st-link_cli.exe" -P "o323BgcFirmwareFiles\o323bgc_v067e_storm32bgc_v110_f103rc.hex" -V
+"bin\ST\STLink\st-link_cli.exe" -Rst
+@pause
