76a5d2bf11eec70cbe15c2fd21bf6af6c3604f63
olliw42 <waldmanns@gmx.de>
2016-03-28 13:18:25 +0200

    release firmware v2.00

---
+++ firmware binaries & gui/o323bgc-release-v200-v20160328/o323BGCTool_Run.bat
@@ -0,0 +1,4 @@
+"bin\ST\STMFlashLoader\STMFlashLoaderOlliW.exe" -c --pn 3 --br 115200 -ow -d --fn "o323BgcNtFirmwareFiles\o323bgc_ntmotor_v022_module_v10_f103t8.hex" --ep --v -r --a 8000000
+
+@echo.
+@pause
