23ce3a3c42052a77ef0494bf0eefc0d729acabe3
olliw42 <waldmanns@gmx.de>
2016-04-12 10:16:55 +0200

    release v2.03

--- firmware binaries & gui/o323bgc-release-v202-v20160408/o323BGCTool_Run.bat
+++
@@ -1,3 +0,0 @@
-"bin\ST\STLink\st-link_cli.exe" -P "o323BgcFirmwareFiles\o323bgc_v202_nt_storm32bgc_v110_f103rc.hex" -V
-"bin\ST\STLink\st-link_cli.exe" -Rst
-@pause
---
+++ firmware binaries & gui/o323bgc-release-v203-v20160412/o323BGCTool_Run.bat
@@ -0,0 +1,3 @@
+"bin\ST\STLink\st-link_cli.exe" -P "o323BgcNtFirmwareFiles\o323bgc_ntimu_v023_module_v10_f103t8.hex" -V
+"bin\ST\STLink\st-link_cli.exe" -Rst
+@pause
