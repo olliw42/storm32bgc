2a1321bc2a33a559cfc7c45823136b5b47637112
olliw42 <waldmanns@gmx.de>
2020-11-15 18:58:01 +0100

    update to 15. Nov. 2020

--- c-library/STorM32_lib.h
+++ c-library/STorM32_lib.h
@@ -1,10 +1,27 @@
 //*****************************************************
 //OW
+// STorM32 C library to handle serial RC commands
+// http://www.olliw.eu/storm32bgc-wiki/Serial_Communication#Serial_Communication_-_RC_Commands
 // (c) olliw, www.olliw.eu, GPL3
+// version 15. Nov. 2020
 //*****************************************************
 //
 // the largest RCcmd response can be 77
 //
+//*****************************************************
+// Example usage for CMD_SETANGLES:
+//    
+// if (_tx_hasspace(sizeof(tSTorM32CmdSetAngles))) {
+//   tSTorM32CmdSetSetAngles t;
+//   t.pitch = 10.0f;
+//   t.roll = 0.0f;
+//   t.yaw = 0.0f;
+//   t.flags = 0;
+//   t.type = 0;
+//   storm32_finalize_CmdSetAngles(&t);
+//   _write( (uint8_t*)(&t), sizeof(tSTorM32CmdSetAngles) );
+// }
+//*****************************************************
 
 #pragma once
 #ifndef STORM32_LIB_H
@@ -20,16 +37,26 @@ extern "C"
 // Constants
 //------------------------------------------------------
 
-// RCcmd data packets, outgoing to STorM32
+#define STORM32RCCMD_HEADER_LEN             3
+#define STORM32RCCMD_CRC_LEN                2
+#define STORM32RCCMD_FRAME_LEN              5
+
+
+// RCCMD data packets, outgoing to STorM32
 enum STORM32RCCMDENUM {
   STORM32RCCMD_GET_VERSIONSTR               = 0x02,
   STORM32RCCMD_GET_DATAFIELDS               = 0x06,
+  STORM32RCCMD_SET_PANMODE                  = 0x0D,
   STORM32RCCMD_DO_CAMERA                    = 0x0F,
   STORM32RCCMD_SET_ANGLES                   = 0x11,
   STORM32RCCMD_SET_PITCHROLLYAW             = 0x12,
+  STORM32RCCMD_SET_PWMOUT                   = 0x13,
   STORM32RCCMD_SET_INPUTS                   = 0x16,
   STORM32RCCMD_SET_HOMELOCATION             = 0x17,
   STORM32RCCMD_SET_TARGETLOCATION           = 0x18,
+  STORM32RCCMD_SET_INPUTCHANNEL             = 0x19,
+  STORM32RCCMD_SET_CAMERA                   = 0x1A,
+  STORM32RCCMD_ACTIVEPANMODESETTING         = 0x64,
   STORM32RCCMD_STORM32LINKV2                = 0xDA,
 };
 
@@ -68,6 +95,25 @@ enum STORM32LIVEDATAENUM {
 };
 
 
+enum STORM32PANMODEENUM {
+    STORM32PANMODE_OFF                      = 0,
+    STORM32PANMODE_HOLDHOLDPAN,
+    STORM32PANMODE_HOLDHOLDHOLD,
+    STORM32PANMODE_PANPANPAN,
+    STORM32PANMODE_PANHOLDHOLD,
+    STORM32PANMODE_PANHOLDPAN,
+    STORM32PANMODE_HOLDPANPAN,
+};
+
+
+enum STORM32DOCAMERAENUM {
+    STORM32DOCAMERA_OFF                      = 0x00,
+    STORM32DOCAMERA_SHUTTER                  = 0x01,
+    STORM32DOCAMERA_VIDEOON                  = 0x03,
+    STORM32DOCAMERA_VIDEOOFF                 = 0x04,
+};
+
+
 enum STORM32LINKFCSTATUSAPENUM {
   STORM32LINK_FCSTATUS_AP_AHRSHEALTHY       = 0x01, //=> Q ok, ca. 15 secs
   STORM32LINK_FCSTATUS_AP_AHRSINITIALIZED   = 0x02, //=> vz ok, ca. 32 secs
@@ -82,7 +128,8 @@ enum STORM32LINKFCSTATUSAPENUM {
 // CRC
 //------------------------------------------------------
 // that's the same as in mavlink/v2.0/checksum.h
-// if that is available, you should include the mavlink version instead of using this here
+// if available, you should include the mavlink version instead of using this here
+// you can force using the crc code here by defining STORM32LIB_USE_CRC before including the lib
 
 #if (defined STORM32LIB_USE_CRC) || (!defined MAVLINK_H)
 
@@ -131,7 +178,7 @@ static inline uint16_t storm32_is_normalstate(uint16_t state)
 
 
 //------------------------------------------------------
-// RCCMD  GetVersionStr  0x02
+// RCCMD  GetVersionStr  #2 = 0x02
 //------------------------------------------------------
 // has a response
 // outgoing: 5 bytes = 434us @ 115200bps
@@ -156,7 +203,7 @@ static inline void storm32_finalize_CmdGetVersionStr(void* buf)
 {
 tSTorM32CmdGetVersionStr* t = (tSTorM32CmdGetVersionStr*)buf;
 
-  t->stx = 0xF9; //0xF9 to suppress response
+  t->stx = 0xFA; //it doesn't make sense to supress the response
   t->len = 0x00;
   t->cmd = 0x02;
   t->crc = crc_calculate(&(t->len), sizeof(tSTorM32CmdGetVersionStr)-3);
@@ -171,7 +218,7 @@ static inline void storm32_finalize_CmdGetVersionStr_(void* buf, uint16_t* len)
 
 
 //------------------------------------------------------
-// RCCMD  GetDataFields  0x06
+// RCCMD  GetDataFields  #6 = 0x06
 //------------------------------------------------------
 // has a response
 // outgoing: 7 bytes = 608us @ 115200bps
@@ -210,7 +257,7 @@ static inline void storm32_finalize_CmdGetDataFields(void* buf)
 {
 tSTorM32CmdGetDataFields* t = (tSTorM32CmdGetDataFields*)buf;
 
-  t->stx = 0xF9; //0xF9 to suppress response
+  t->stx = 0xFA; //it doesn't make sense to supress the response
   t->len = 0x02;
   t->cmd = 0x06;
   t->crc = crc_calculate(&(t->len), sizeof(tSTorM32CmdGetDataFields)-3);
@@ -225,7 +272,42 @@ static inline void storm32_finalize_CmdGetDataFields_(void* buf, uint16_t* len)
 
 
 //------------------------------------------------------
-// RCCMD  DoCamera  0x0F
+// RCCMD  SetPanMode  #13 = 0x0D
+//------------------------------------------------------
+// only outgoing, has no response (except of an ACK)
+// outgoing: 6 bytes = 521us @ 115200bps
+
+#define STORM32RCCMD_SET_PANMODE_OUTLEN       0x01
+
+struct STORM32LIBPACKED tSTorM32CmdSetPanMode { //len = 0x01
+  uint8_t stx;
+  uint8_t len;
+  uint8_t cmd;
+  uint8_t panmode;
+  uint16_t crc;
+};
+
+
+static inline void storm32_finalize_CmdSetPanMode(void* buf)
+{
+tSTorM32CmdSetPanMode* t = (tSTorM32CmdSetPanMode*)buf;
+
+  t->stx = 0xF9; //0xF9 to suppress response
+  t->len = 0x01;
+  t->cmd = 0x0D;
+  t->crc = crc_calculate(&(t->len), sizeof(tSTorM32CmdSetPanMode)-3);
+}
+
+
+static inline void storm32_finalize_CmdSetPanMode_(void* buf, uint16_t* len)
+{
+  storm32_finalize_CmdSetPanMode(buf);
+  *len = sizeof(tSTorM32CmdSetPanMode);
+}
+
+
+//------------------------------------------------------
+// RCCMD  DoCamera  #15 = 0x0F
 //------------------------------------------------------
 // only outgoing, has no response (except of an ACK)
 // outgoing: 11 bytes = 955us @ 115200bps
@@ -265,7 +347,7 @@ static inline void storm32_finalize_CmdDoCamera_(void* buf, uint16_t* len)
 
 
 //------------------------------------------------------
-// RCCMD  SetAngles  0x11
+// RCCMD  SetAngles  #17 = 0x11
 //------------------------------------------------------
 // only outgoing, has no response (except of an ACK)
 // outgoing: 19 bytes = 1650us @ 115200bps
@@ -302,15 +384,15 @@ static inline void storm32_finalize_CmdSetAngles_(void* buf, uint16_t* len)
   *len = sizeof(tSTorM32CmdSetAngles);
 }
 
-    
+
 //------------------------------------------------------
-// RCCMD  SetPitchRollYaw  0x12
+// RCCMD  SetPitchRollYaw  #18 = 0x12
 //------------------------------------------------------
 // only outgoing, has no response (except of an ACK)
 // outgoing: 11 bytes = 955us @ 115200bps
 
 #define STORM32RCCMD_SET_PITCHROLLYAW_OUTLEN    0x06
-    
+
 struct STORM32LIBPACKED tSTorM32CmdSetPitchRollYaw { //len = 0x06
   uint8_t stx;
   uint8_t len;
@@ -326,7 +408,7 @@ static inline void storm32_finalize_CmdSetPitchRollYaw(void* buf)
 {
 tSTorM32CmdSetPitchRollYaw* t = (tSTorM32CmdSetPitchRollYaw*)buf;
 
-  t->stx = 0xF9; //0xFA; //0xF9 to suppress response
+  t->stx = 0xF9; //0xF9 to suppress response
   t->len = 0x06;
   t->cmd = 0x12;
   t->crc = crc_calculate(&(t->len), sizeof(tSTorM32CmdSetPitchRollYaw)-3);
@@ -339,9 +421,44 @@ static inline void storm32_finalize_CmdSetPitchRollYaw_(void* buf, uint16_t* len
   *len = sizeof(tSTorM32CmdSetPitchRollYaw);
 }
 
-    
+
 //------------------------------------------------------
-// RCCMD  SetInputs  0x16
+// RCCMD  SetPwmOut  #19 = 0x13
+//------------------------------------------------------
+// only outgoing, has no response (except of an ACK)
+// outgoing: 7 bytes = 608us @ 115200bps
+
+#define STORM32RCCMD_SET_PWMOUT_OUTLEN    0x02
+
+struct STORM32LIBPACKED tSTorM32CmdSetPwmOut { //len = 0x02
+  uint8_t stx;
+  uint8_t len;
+  uint8_t cmd;
+  uint16_t pwm;
+  uint16_t crc;
+};
+
+
+static inline void storm32_finalize_CmdSetPwmOut(void* buf)
+{
+tSTorM32CmdSetPwmOut* t = (tSTorM32CmdSetPwmOut*)buf;
+
+  t->stx = 0xF9; //0xF9 to suppress response
+  t->len = 0x02;
+  t->cmd = 0x13;
+  t->crc = crc_calculate(&(t->len), sizeof(tSTorM32CmdSetPwmOut)-3);
+}
+
+
+static inline void storm32_finalize_CmdSetPwmOut_(void* buf, uint16_t* len)
+{
+  storm32_finalize_CmdSetPwmOut(buf);
+  *len = sizeof(tSTorM32CmdSetPwmOut);
+}
+
+
+//------------------------------------------------------
+// RCCMD  SetInputs  #22 = 0x16
 //------------------------------------------------------
 // only outgoing, has no response (except of an ACK)
 // outgoing: 28 bytes = 2431us @ 115200bps
@@ -392,7 +509,7 @@ static inline void storm32_finalize_CmdSetInputs_(void* buf, uint16_t* len)
 
 
 //------------------------------------------------------
-// RCCMD  SetHomeTargetLocation  0x17, 0x18
+// RCCMD  SetHomeTargetLocation  #23, 24 = 0x17, 0x18
 //------------------------------------------------------
 // only outgoing, has no response (except of an ACK)
 // cmd = 0x17 for home, 0x18 for target
@@ -400,7 +517,7 @@ static inline void storm32_finalize_CmdSetInputs_(void* buf, uint16_t* len)
 
 #define STORM32RCCMD_SET_HOMELOCATION_OUTLEN    0x0E
 #define STORM32RCCMD_SET_TARGETLOCATION_OUTLEN  0x0E
-    
+
 struct STORM32LIBPACKED tSTorM32CmdSetHomeTargetLocation { //len = 0x0E
   uint8_t stx;
   uint8_t len;
@@ -450,7 +567,114 @@ static inline void storm32_finalize_CmdSetTargetLocation_(void* buf, uint16_t* l
 
 
 //------------------------------------------------------
-// RCCMD  STorM32LinkV2  0xDA
+// RCCMD  SetInputChannel  #25 = 0x19
+//------------------------------------------------------
+// only outgoing, has no response (except of an ACK)
+// outgoing: 9 bytes = 781us @ 115200bps
+
+#define STORM32RCCMD_SET_INPUTCHANNEL_OUTLEN    0x04
+
+struct STORM32LIBPACKED tSTorM32SetInputChannel { //len = 0x04
+  uint8_t stx;
+  uint8_t len;
+  uint8_t cmd;
+  uint16_t channel;
+  uint16_t pwm;
+  uint16_t crc;
+};
+
+
+static inline void storm32_finalize_CmdSetInputChannel(void* buf)
+{
+tSTorM32SetInputChannel* t = (tSTorM32SetInputChannel*)buf;
+
+  t->stx = 0xF9; //0xF9 to suppress response
+  t->len = 0x04;
+  t->cmd = 0x1A;
+  t->crc = crc_calculate(&(t->len), sizeof(tSTorM32SetInputChannel)-3);
+}
+
+
+static inline void storm32_finalize_CmdSetInputChannel_(void* buf, uint16_t* len)
+{
+  storm32_finalize_CmdSetInputChannel(buf);
+  *len = sizeof(tSTorM32SetInputChannel);
+}
+
+
+//------------------------------------------------------
+// RCCMD  SetCamera  #26 = 0x1A
+//------------------------------------------------------
+// only outgoing, has no response (except of an ACK)
+// outgoing: 9 bytes = 781us @ 115200bps
+
+#define STORM32RCCMD_SET_SETCAMERA_OUTLEN    0x04
+
+struct STORM32LIBPACKED tSTorM32SetCamera { //len = 0x04
+  uint8_t stx;
+  uint8_t len;
+  uint8_t cmd;
+  uint16_t control;
+  uint16_t control2;
+  uint16_t crc;
+};
+
+
+static inline void storm32_finalize_CmdSetCamera(void* buf)
+{
+tSTorM32SetCamera* t = (tSTorM32SetCamera*)buf;
+
+  t->stx = 0xF9; //0xF9 to suppress response
+  t->len = 0x04;
+  t->cmd = 0x1A;
+  t->crc = crc_calculate(&(t->len), sizeof(tSTorM32SetCamera)-3);
+}
+
+
+static inline void storm32_finalize_CmdSetCamera_(void* buf, uint16_t* len)
+{
+  storm32_finalize_CmdSetCamera(buf);
+  *len = sizeof(tSTorM32SetCamera);
+}
+
+
+//------------------------------------------------------
+// RCCMD  ActivePanModeSetting  #100 = 0x62
+//------------------------------------------------------
+// only outgoing, has no response (except of an ACK)
+// outgoing: 6 bytes = 521us @ 115200bps
+
+#define STORM32RCCMD_ACTIVEPANMODESETTING_OUTLEN    0x01
+
+struct STORM32LIBPACKED tSTorM32ActivePanModeSetting { //len = 0x01
+  uint8_t stx;
+  uint8_t len;
+  uint8_t cmd;
+  uint8_t activepanmodesetting;
+  uint16_t crc;
+};
+
+
+static inline void storm32_finalize_CmdActivePanModeSetting(void* buf)
+{
+tSTorM32ActivePanModeSetting* t = (tSTorM32ActivePanModeSetting*)buf;
+
+  t->stx = 0xF9; //0xF9 to suppress response
+  t->len = 0x01;
+  t->cmd = 0x64;
+  t->crc = crc_calculate(&(t->len), sizeof(tSTorM32ActivePanModeSetting)-3);
+}
+
+
+static inline void storm32_finalize_CmdActivePanModeSetting_(void* buf, uint16_t* len)
+{
+  storm32_finalize_CmdActivePanModeSetting(buf);
+  *len = sizeof(tSTorM32ActivePanModeSetting);
+}
+
+
+//------------------------------------------------------
+// RCCMD  STorM32LinkV2  #218 = 0xDA
 //------------------------------------------------------
 // only outgoing, has no response (except of an ACK)
 // outgoing: 33 bytes = 2865us @ 115200bps
